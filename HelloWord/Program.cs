92cc7d98019d2efcde4b39af8e69b3814d903c46
Leonardo F Garcia <leonanet92@gmail.com>
2024-09-10 23:09:27 -0300

    !!duas

--- HelloWord/Program.cs
+++ HelloWord/Program.cs
@@ -3,6 +3,6 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("ola mundo!!" );
-        Console.WriteLine("o mundo mudou" );
+        Console.WriteLine("o mundo mudou!!" );
     }
 }
